4fcb3f40e145364962c5d4b1a8b5b9079a364cd4
AKorezin <source-email@yandex.ru>
2014-02-22 10:30:01 +0400

    seems script works ok

--- makeme.sh
+++ makeme.sh
@@ -1,4 +1,3 @@
-cd ./AKorezin/spring/
 #find . -type d -name "task*" | while read path
 #do
 #	cd $path
@@ -8,14 +7,21 @@ cd ./AKorezin/spring/
 #	cd ..
 #done
 
+cd ./AKorezin/spring/
+i=0
 for path in task*; do
 	if [ -e "$path" ]
 	then
-		echo "$path"
+		cd "$path"
+		make
+		./app*
+		i=$((i+$?))
+		make clean
+		cd ..
 	else
-		echo "nothing"
-		exit 0
+		echo "nothing happend"
+		exit 1
 	fi
 done
-exit 0
+exit "$i"
 
