1b8df6d48e1ab59bea45487b7b05f10d3abf0129
AKorezin <source-email@yandex.ru>
2014-03-05 20:00:22 +0400

    task_8 template

---
+++ AKorezin/spring/task_8/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <list>
+int main(int argc,char **argv)
+{
+	srand(time(NULL));
+	int i,n=10;
+	std::list <int> a;
+	std::list::iterator it;
+	for(i=0;i<n;i++)
+		a.push_front(rand()%20);
+	for(it=a.begin();it!=a.end();it++)
+	{
+		std::cout<<*it<<" ";
+	}
+	std::cout<<std::endl;
+	for(it=a.begin();it!=a.end();it++)
+	{
+		std::cout<<*it<<" ";
+	}
+	std::cout<<std::endl;
+}
