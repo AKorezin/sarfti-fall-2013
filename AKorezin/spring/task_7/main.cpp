34436d8c1c59117945faf7d8934a773f6bbbc420
AKorezin <source-email@yandex.ru>
2014-03-05 19:33:32 +0400

    insertion sort

--- AKorezin/spring/task_7/main.cpp
+++ AKorezin/spring/task_7/main.cpp
@@ -12,7 +12,15 @@ int main(int argc,char **argv)
 	for(i=0;i<n;i++)
 		std::cout<<a[i]<<" ";
 	std::cout<<std::endl;
-
+	int x;
+	for(i=1;i<n;i++)
+	{
+		x=a[i];
+		int j;
+		for(j=i-1;j>=0 && x>a[j];j--)
+			a[j+1]=a[j];
+		a[j+1]=x;
+	}
 	
 
 	for(i=0;i<n;i++)
