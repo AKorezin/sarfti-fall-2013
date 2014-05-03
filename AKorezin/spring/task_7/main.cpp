9945c06972fe13b1b25685b786d21fc5eaf783ef
AKorezin <source-email@yandex.ru>
2014-05-03 13:16:19 +0400

    incapsulation upgrade

--- AKorezin/spring/task_7/main.cpp
+++ AKorezin/spring/task_7/main.cpp
@@ -3,27 +3,35 @@
 #include <cstdlib>
 int main(int argc,char **argv)
 {
+	void sort(int *,int);
 	srand(time(NULL));
 	int i,n=10;
+	if (argc=2)
+		n=atoi(argv[1]);
 	int a[n];
 	for(i=0;i<n;i++)
-		a[i]=rand()%20;
+		a[i]=rand()%50;
 	
 	for(i=0;i<n;i++)
 		std::cout<<a[i]<<" ";
 	std::cout<<std::endl;
+	sort(a,n);
+	for(i=0;i<n;i++)
+		std::cout<<a[i]<<" ";
+	std::cout<<std::endl;
+	return 0;
+}
+
+void sort(int * a,int n){
 	int x;
-	for(i=1;i<n;i++)
+
+	for(int i=1;i<n;i++)
 	{
 		x=a[i];
 		int j;
-		for(j=i-1;j>=0 && x>a[j];j--)
+		for(j=i-1;j>=0 && x<a[j];j--)
 			a[j+1]=a[j];
 		a[j+1]=x;
 	}
 	
-
-	for(i=0;i<n;i++)
-		std::cout<<a[i]<<" ";
-	std::cout<<std::endl;
 }
--- AKorezin/spring/task_7/run.sh
+++ AKorezin/spring/task_7/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo ./app* 
-./app*
+./app* 20
